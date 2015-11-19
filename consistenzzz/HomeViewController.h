49a0fbf73b9ec700dd9bd7a01547becf003a3bb4
natelohn <nlohn@stanford.edu>
2015-11-19 04:17:22 -0800

    merging setup and home. not successful

--- consistenzzz/HomeViewController.h
+++ consistenzzz/HomeViewController.h
@@ -9,20 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface HomeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIDatePicker *sleepTimePicker;
 
-
-//@property (weak, nonatomic) IBOutlet UIDatePicker *sleepTimePicker;
-//
-//@property (weak, nonatomic) IBOutlet UILabel *label1;
-//
-//@property (weak, nonatomic) IBOutlet UIButton *sleepTimeButton;
-//@property (weak, nonatomic) IBOutlet UIButton *wakeTimeButton;
-//
-//@property (weak, nonatomic) IBOutlet UILabel *sleepTimeLabel;
-//@property (weak, nonatomic) IBOutlet UILabel *wakeTimeLabel;
-//
-//@property (weak, nonatomic) IBOutlet UILabel *sleepTime;
-//@property (weak, nonatomic) IBOutlet UILabel *wakeTime;
-
+@property (weak, nonatomic) IBOutlet UIButton *sleepTimeButton;
+@property (weak, nonatomic) IBOutlet UIButton *wakeTimeButton;
+@property (weak, nonatomic) IBOutlet UILabel *sleepTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *wakeTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sleepTime;
+@property (weak, nonatomic) IBOutlet UILabel *wakeTime;
 
 @end
---
+++ consistenzzz/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  consistenzzz
+//
+//  Created by Nate Lohn on 11/19/15.
+//  Copyright © 2015 the dream team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
